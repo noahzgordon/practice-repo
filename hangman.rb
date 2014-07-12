7ff628c05a65cca01fc78895e4f4c6a759da6d68
Noah Gordon <tortuga.ng@gmail.com>
2014-07-12 18:33:59 -0400

    Implement working version if human is guessing.

--- hangman.rb
+++ hangman.rb
@@ -13,26 +13,27 @@ class Hangman
   end
   
   def play
+    
     length = referee.pick_secret_word.to_i
     @partial_word = '*' * length
     
     guesser.receive_secret_length(length)
     
     until won?
-      guess = guesser.guess(wrong_guesses)
+      @guess = guesser.guess(wrong_guesses)
     
       correct_places = referee.check_guess(guess)
       correct = correct_places.empty? ? false : true
       
       update_word(correct_places)
       
-      guesser.handle_response(partial_word, guess, correct)
+      guesser.handle_response(partial_word, @guess, correct)
     end
     
-    if winner == guesser
-      puts "Guesser wins!"
-    else
+    if winner == :referee
       puts "Referee wins!"
+    else
+      puts "Guesser wins!"
     end
     
     # puts "Play again? (y/n)"
@@ -57,8 +58,7 @@ class Hangman
   end
   
   def winner
-    guesser unless partial_word.chars.include?('*')
-    referee if wrong_guesses >= 7
+    :referee if wrong_guesses >= 7
   end
   
   def update_word(indices)
@@ -87,8 +87,8 @@ class ComputerPlayer
   def check_guess(guess)
     correct_positions = []
     
-    guess.chars.each_with_index do |char, i|
-      correct_positions << i if char == secret_word[i]
+    @secret_word.chars.each_with_index do |char, i|
+      correct_positions << i if char == guess
     end
     
     correct_positions
@@ -104,7 +104,7 @@ class ComputerPlayer
   def guess(ignore)
     most_common_letters = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z']
   
-    if @trimmed_dictionary.count < 5000
+    if @trimmed_dictionary.count < 1000
       make_smart_guess
     else
       most_common_letters.find { |letter| !@guessed_letters.include?(letter)}
@@ -126,20 +126,20 @@ class ComputerPlayer
   def cut_dictionary_when_right(partial_word)
     # takes only those words which leave only asterisks
     # when their characters are subtracted from the partial word
-    trimmed_dictionary.select! do |word|
+    @trimmed_dictionary.select! do |word|
       (partial_word.chars - word.chars).all? { |char| char == '*' }
     end
   end
   
   def cut_dictionary_when_wrong(guess)
     # removes all words that contain the incorrect guess
-    trimmed_dictionary.select! { |word| !word.include? guess }
+    @trimmed_dictionary.select! { |word| !word.include? guess }
   end
   
   def make_smart_guess
     # guesses the letter most-represented among the words left 
-    dict_chars = trimmed_dictionary.to_a.join.split('')
-    dict_chars.select! { |char| !guessed_letters.include?(char) }
+    dict_chars = @trimmed_dictionary.to_a.join.split('')
+    dict_chars.select! { |char| !@guessed_letters.include?(char) }
     
     guess = dict_chars.max_by { |char| dict_chars.count(char) }
     
@@ -188,8 +188,16 @@ class HumanPlayer
     puts "#{7 - wrong_guesses} guesses remaining!"
     
     puts "What is your guess?"
-    guess = gets.chomp
+    guess = gets.chomp.downcase
     
+    if @guessed_letters.include? (guess)
+      puts "You already guessed that! Try again."
+      guess(wrong_guesses)
+    end
+    
+    @guessed_letters << guess
+    
+    guess
   end
   
   def handle_response(partial_word, guess, correct)
