9c9c46d5a009e2eaed086411fa895f24b9f66646
Noah Gordon <tortuga.ng@gmail.com>
2014-07-12 20:24:23 -0400

    Implement much faster adjacent_words method

--- word_chains.rb
+++ word_chains.rb
@@ -2,21 +2,53 @@ require 'set'
 
 class WordChainer
   
-  attr_reader :dictionary, :trimmed_dictionary
+  attr_reader :dictionary
+  attr_accessor :current_words, :all_seen_words
   
   def initialize
     @dictionary = Set.new(File.readlines('dictionary.txt').map(&:chomp))
   end
   
+  def run(source, target)
+    current_words = [source]
+    all_seen_words = [source]
+    
+  end
+  
+  private
+  
   def adjacent_words(word)
-    trimmed = dictionary.select do |dict_word| 
-      match_count = 0
-      dict_word.chars.each_with_index do |char, i|
-        match_count += 1 if char == word[i]
+    adj_words = []
+    
+    word.chars.each_with_index do |char, i|
+      ('a'..'z').to_a.each do |alph|
+        new_word = word.dup
+        
+        new_word[i] = alph unless word[i] == alph
+        adj_words << new_word if dictionary.include? new_word
       end
-      dict_word.length == word.length && match_count == word.length - 1
     end
     
-    @trimmed_dictionary = Set.new(trimmed)
+    adj_words
   end
+  
+  def explore_current_words
+    until current_words.empty?
+      new_current_words = []
+      
+      current_words.each do |cur_word|
+        
+        adjacent_words(cur_word).each do |adj_word|
+          next if all_seen_words.include? adj_word
+          
+          new_current_words << adj_word
+          all_seen_words << adj_word
+        end
+      end
+      
+      p new_current_words
+      current_words = new_current_words
+    end
+  end
+ 
 end
